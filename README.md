5b486f05372061fa81fa765636419f8a0d81c2d5
Ali <aliwhitrow1@gmail.com>
2023-11-15 04:18:17 +0000

    few minor changes made to simple program

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # simple-captcha-example
+
+This example uses react-simple-captcha (https://www.npmjs.com/package/react-simple-captcha) as an example for provide a thin layer of security against bots.
+
+## How to...
+
+clone the repo
+
+    git clone https://github.com/AliCW/simple-captcha-example.git
+
+Initialize npm for react install
+
+    npm init
+
+Install react
+
+    npm install react
+
+Create a react app
+
+    npx create-react-app <your-app-name>
+
+Install dependencies
+
+    npm install
+
+Run the program by navigating to the site root at /simple-captcha-test/ and type the below
+
+    npm start
+
+## Colour Modification
+
+You can change the colour of your CAPTCHA code by altering the hex code next to the 'loadCaptchaEnginge' call
+
--- simple-captcha-test/src/Components/Captcha.jsx
+++ simple-captcha-test/src/Components/Captcha.jsx
@@ -8,50 +8,59 @@ export default function Captcha() {
     loadCaptchaEnginge(10, '#005812', '#103b19')
   }, []);
 
-  const doSubmit = (event) => {
-    event.preventDefault();
+  const doSubmit = () => {
+
     let user_captcha = document.getElementById('user_captcha_input').value;
 
     if (validateCaptcha(user_captcha) === true) {
-        loadCaptchaEnginge(10); 
+        alert('Captcha Correct');
+        loadCaptchaEnginge(10, '#005812', '#103b19'); 
         document.getElementById('user_captcha_input').value = "";
     }
     else {
         alert('Captcha Failed To Match');
         document.getElementById('user_captcha_input').value = "";
-        loadCaptchaEnginge(10);
+        loadCaptchaEnginge(10, '#005812', '#103b19');
         window.location.reload(false);
-    }
+    };
     return;
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     doSubmit();
-}
+};
 
   return (
-    <div>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'center',
+    }}>
       <form onSubmit={handleSubmit}>
-
-        <div>
+        <div style={{
+          margin: "20px",
+          padding: "20px",
+        }}>
           <LoadCanvasTemplate />
         </div>
         <div>
-        <textarea
-            id="contactPage"
-            placeholder="Name / Title*"
+          <textarea
+            id="user_captcha_input"
+            placeholder="Enter CAPTCHA*"
             rows="1"
             className="user-text-area-title"
             name="from_name"
             onChange={(event) => event.target.value}
           ></textarea>
         </div>
-        <div>
-            <button className="submit-button">Send</button>
-          </div>
+        <div style={{
+          margin: "20px",
+        }}>
+          <button className="submit-button">Send</button>
+        </div>
       </form>
     </div>
-  )
-
-}
+  );
+};
